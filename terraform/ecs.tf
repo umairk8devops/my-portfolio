1a2417f6802f91cbcb8d02536a46e9df29bf07c5
umairk8devops <90587224+umairk8devops@users.noreply.github.com>
2025-10-15 14:20:59 -0500

    fixing the port on lb

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -240,6 +240,7 @@ resource "aws_lb_target_group" "portfolio" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.existing.id
+  target_type = "instance"
 
   health_check {
     enabled             = true
